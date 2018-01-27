994897e142ba50cb752f4d5f916e1846b7d5c7ef
Alexandre Teyar <alexandre.teyar@gmail.com>
2018-01-27 14:50:37 +0000

    rework post data feature

--- src/main/java/copy_as_powershell_requests/utils/ExtensionHelpers.java
+++ src/main/java/copy_as_powershell_requests/utils/ExtensionHelpers.java
@@ -20,6 +20,7 @@ import burp.IBurpExtenderCallbacks;
 import burp.IHttpRequestResponse;
 import burp.IParameter;
 import burp.IRequestInfo;
+import java.util.Arrays;
 import java.util.List;
 import org.apache.commons.text.StringEscapeUtils;
 
@@ -28,10 +29,8 @@ public class ExtensionHelpers {
   private IBurpExtenderCallbacks callbacks;
   private int maximumRedirection = 0;
   private boolean hasContentType;
-  private boolean hasParamBody;
-  private boolean hasParamJson;
-  private boolean hasParamMultipart;
-  private boolean hasParamUrl;
+  private boolean hasBodyParams;
+  private boolean hasURLParams;
   private boolean hasUserAgent;
 
   public ExtensionHelpers(IBurpExtenderCallbacks callbacks) {
@@ -42,21 +41,23 @@ public class ExtensionHelpers {
       IHttpRequestResponse selectedMessage) {
     IRequestInfo requestInfo = this.callbacks.getHelpers().analyzeRequest(selectedMessage);
     StringBuilder stringBuilder = new StringBuilder();
-    // prevents OS command injection
     String method = StringEscapeUtils.builder(StaticData.ESCAPE_POWERSHELL)
         .escape(requestInfo.getMethod()).toString();
-    String uri = StringEscapeUtils.builder(StaticData.ESCAPE_POWERSHELL)
+    String URI = StringEscapeUtils.builder(StaticData.ESCAPE_POWERSHELL)
         .escape(requestInfo.getUrl().toString()).toString();
 
-    stringBuilder.append("$method = [Microsoft.PowerShell.Commands.WebRequestMethod]::" + method)
-        .append(System.lineSeparator()).append("$uri = [System.Uri]::new(\"" + uri + "\")")
-        .append(System.lineSeparator())
-        .append("$maximumRedirection = [System.Int32]" + this.maximumRedirection)
+    stringBuilder.append("$method = [Microsoft.PowerShell.Commands.WebRequestMethod]::")
+        .append(method)
+        .append(System.lineSeparator()).append("$URI = [System.Uri]::new(\"").append(URI)
+        .append("\")")
+        .append(System.lineSeparator()).append("$maximumRedirection = [System.Int32]")
+        .append(this.maximumRedirection)
         .append(System.lineSeparator());
     stringBuilder.append(processHeaders(requestInfo.getHeaders()));
-    stringBuilder.append(processParameters(requestInfo.getParameters()));
+    stringBuilder.append(processURL(requestInfo.getParameters()));
+    stringBuilder.append(processBody(selectedMessage, requestInfo));
     stringBuilder.append(
-        "Invoke-WebRequest -Method $method -Uri $uri -MaximumRedirection $maximumRedirection -Headers $headers");
+        "Invoke-WebRequest -Method $method -Uri $URI -MaximumRedirection $maximumRedirection -Headers $headers");
 
     if (this.hasContentType) {
       stringBuilder.append(" -ContentType $contentType");
@@ -66,35 +67,19 @@ public class ExtensionHelpers {
       stringBuilder.append(" -UserAgent $userAgent");
     }
 
-    if (this.hasParamBody) {
+    if (this.hasBodyParams) {
       if (!stringBuilder.toString().contains("-Body")) {
-        stringBuilder.append(" -Body $paramBody");
+        stringBuilder.append(" -Body $BodyParams");
       } else {
-        stringBuilder.append(", $paramBody");
+        stringBuilder.append(", $BodyParams");
       }
     }
 
-    if (this.hasParamJson) {
+    if (this.hasURLParams) {
       if (!stringBuilder.toString().contains("-Body")) {
-        stringBuilder.append(" -Body $paramJson");
+        stringBuilder.append(" -Body $URLParams");
       } else {
-        stringBuilder.append(", $paramJson");
-      }
-    }
-
-//    if (this.hasParamMultipart) {
-//      if (!stringBuilder.toString().contains("-Body")) {
-//        stringBuilder.append(" -Body $paramMultipart");
-//      } else {
-//        stringBuilder.append(", $paramMultipart");
-//      }
-//    }
-
-    if (this.hasParamUrl) {
-      if (!stringBuilder.toString().contains("-Body")) {
-        stringBuilder.append(" -Body $paramUrl");
-      } else {
-        stringBuilder.append(", $paramUrl");
+        stringBuilder.append(", $URLParams");
       }
     }
 
@@ -110,7 +95,6 @@ public class ExtensionHelpers {
 
     // skip the first header line
     for (String header : headers.subList(1, headers.size())) {
-      // prevents OS command injection
       String headerName = StringEscapeUtils.builder(StaticData.ESCAPE_POWERSHELL)
           .escape((header.split(": ")[0] + "")).toString();
       String headerValue = StringEscapeUtils.builder(StaticData.ESCAPE_POWERSHELL)
@@ -141,87 +125,56 @@ public class ExtensionHelpers {
     return stringBuilder;
   }
 
-  private StringBuilder processParameters(List<IParameter> parameters) {
-    this.hasParamBody = false;
-    this.hasParamJson = false;
-    this.hasParamMultipart = false;
-    this.hasParamUrl = false;
-    boolean firstParamBody = true;
-    boolean firstParamJson = true;
-    boolean firstParamMultipart = true;
-    boolean firstParamUrl = true;
+  private StringBuilder processURL(List<IParameter> parameters) {
+    this.hasURLParams = false;
+    boolean isFirstIteration = true;
     StringBuilder stringBuilder = new StringBuilder();
 
     if (!parameters.isEmpty()) {
       for (IParameter parameter : parameters) {
-        // prevents OS command injection
         String parameterName = StringEscapeUtils.builder(StaticData.ESCAPE_POWERSHELL)
             .escape(parameter.getName()).toString();
         String parameterValue = StringEscapeUtils.builder(StaticData.ESCAPE_POWERSHELL)
             .escape(parameter.getValue()).toString();
 
         switch (parameter.getType()) {
-          case IParameter.PARAM_BODY:
-            if (firstParamBody) {
-              this.hasParamBody = true;
-              stringBuilder.append(
-                  "$paramBody = [System.Collections.Generic.Dictionary[string,string]]::new()")
-                  .append(System.lineSeparator());
-              firstParamBody = false;
-            }
-
-            stringBuilder.append(
-                "$paramBody.Add(\"" + parameterName + "\", \"" + parameterValue
-                    + "\")")
-                .append(System.lineSeparator());
-            break;
-          case IParameter.PARAM_JSON:
-            if (firstParamJson) {
-              this.hasParamJson = true;
-              stringBuilder.append(
-                  "$paramJson = [System.Collections.Generic.Dictionary[string,string]]::new()")
-                  .append(System.lineSeparator());
-              firstParamJson = false;
-            }
-
-            stringBuilder.append(
-                "$paramJson.Add(\"" + parameterName + "\", \"" + parameterValue
-                    + "\")")
-                .append(System.lineSeparator());
-            break;
           case IParameter.PARAM_URL:
-            if (firstParamUrl) {
-              this.hasParamUrl = true;
+            if (isFirstIteration) {
+              this.hasURLParams = true;
               stringBuilder.append(
-                  "$paramUrl = [System.Collections.Generic.Dictionary[string,string]]::new()")
+                  "$URLParams = [System.Collections.Generic.Dictionary[string,string]]::new()")
                   .append(System.lineSeparator());
-              firstParamUrl = false;
+              isFirstIteration = false;
             }
 
             stringBuilder.append(
-                "$paramUrl.Add(\"" + parameterName + "\", \"" + parameterValue
+                "$URLParams.Add(\"" + parameterName + "\", \"" + parameterValue
                     + "\")")
                 .append(System.lineSeparator());
-            break;
-          case IParameter.PARAM_MULTIPART_ATTR:
-//            if (firstParamMultipart) {
-//              this.hasParamMultipart = true;
-//              stringBuilder.append(
-//                  "$paramMultipart = [System.Net.Http.MultipartFormDataContent]::new()")
-//                  .append(System.lineSeparator());
-//              firstParamMultipart = false;
-//            }
-//
-//            stringBuilder.append(parameterName + " --- " + parameterValue)
-//                .append(System.lineSeparator());
             break;
           default:
-            callbacks
-                .printError(
-                    "Please raise a new issue on https://github.com/AresS31/copy_as_powershell_requests.");
             break;
         }
       }
+    }
+
+    return stringBuilder;
+  }
+
+  private StringBuilder processBody(IHttpRequestResponse selectedMessage,
+      IRequestInfo requestInfo) {
+    this.hasBodyParams = false;
+    int bodyOffset = requestInfo.getBodyOffset();
+    byte[] request = selectedMessage.getRequest();
+    StringBuilder stringBuilder = new StringBuilder();
+
+    if (request.length > bodyOffset) {
+      this.hasBodyParams = true;
+      String postData = StringEscapeUtils.builder(StaticData.ESCAPE_POWERSHELL).escape(
+          this.callbacks.getHelpers()
+              .bytesToString(Arrays.copyOfRange(request, bodyOffset, request.length))).toString();
+      stringBuilder.append("$bodyParams = [System.String]::new(\"").append(postData).append("\")")
+          .append(System.lineSeparator());
     }
 
     return stringBuilder;
